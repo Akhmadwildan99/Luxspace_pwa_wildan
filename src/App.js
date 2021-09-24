bd855a2c06f80cb701b2d3fcdb29cb65eedd373f
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-09-24 21:54:47 +0700

    menambah component AsideMenu Clients Footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import Header from './Components/Header';
 import Hero from './Components/Hero';
 import Browse from './Components/Browse';
-import Arived from './Components/Arived'
+import Arived from './Components/Arived';
+import Clients from './Components/Clients';
+import AsideMenu from './Components/AsideMenu';
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Hero />
       <Browse />
       <Arived />
+      <Clients />
+      <AsideMenu />
+      <Footer />
     </>
   );
 }
