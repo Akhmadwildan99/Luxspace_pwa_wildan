a663798f5ba177ac234b269aa2511625b9656b4f
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-09-29 17:06:41 +0700

    membaca data json dari api

--- src/App.js
+++ src/App.js
@@ -8,12 +8,27 @@ import AsideMenu from './Components/AsideMenu';
 import Footer from './Components/Footer';
 
 function App() {
+  const [items, setItems] = React.useState([]);
+  React.useEffect(function() {
+    (async function() {
+      const response = await fetch('https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc',{
+        headers: {
+          'Content-Type': 'application-json',
+          'accept': 'application-json',
+          'x-api-key': process.env.REACT_APP_APIKEY
+        }
+      });
+      const {nodes} = await response.json();
+      setItems(nodes)
+      console.log(nodes)
+    })();
+  }, [])
   return (
     <>
       <Header />
       <Hero />
       <Browse />
-      <Arived />
+      <Arived items={items}/>
       <Clients />
       <AsideMenu />
       <Footer />
